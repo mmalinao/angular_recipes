c67f5ce6f66c9326ebf82f4080564d71b65bda04
Matt Malinao <matt.malinao@gmail.com>
2015-01-07 12:20:16 -0700

    recipe grape entity

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,7 @@
 class Recipe < ActiveRecord::Base
+  include Grape::Entity::DSL
+
   validates :name, presence: true
+
+  entity :id, :name, :instructions
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe Recipe, type: :model do
   it { is_expected.to validate_presence_of :name }
+
+  describe Recipe::Entity do
+    subject { Recipe::Entity }
+
+    it { is_expected.to represent :id }
+    it { is_expected.to represent :name }
+    it { is_expected.to represent :instructions }
+  end
 end

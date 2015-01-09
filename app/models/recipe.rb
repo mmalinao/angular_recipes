d499bc9635d763face3ab503fb5c1db10da035de
Matt Malinao <matt.malinao@gmail.com>
2015-01-08 19:09:50 -0700

    recipe keywords scope

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,9 @@ class Recipe < ActiveRecord::Base
   validates :name, presence: true
 
   entity :id, :name, :instructions
+
+  def self.keywords(str)
+    arr = str.split(' ').map{ |x| "%#{x}%" }
+    Recipe.where('name ilike any ( array[?] )', arr)
+  end
 end
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -10,4 +10,31 @@ RSpec.describe Recipe, type: :model do
     it { is_expected.to represent :name }
     it { is_expected.to represent :instructions }
   end
+
+  describe '.keyword' do
+    subject { Recipe.keywords(str) }
+
+    let!(:recipe_1) { FactoryGirl.create(:recipe, name: 'Baked Potato') }
+    let!(:recipe_2) { FactoryGirl.create(:recipe, name: 'Brussel Sprouts') }
+
+    context 'when recipe exist with matching keyword' do
+      let(:str) { 'baked' }
+
+      it 'should include matching recipe' do
+        is_expected.to include recipe_1
+      end
+
+      it 'should exclude other recipes' do
+        is_expected.to_not include recipe_2
+      end
+    end
+
+    context 'when multiple recipes exist with matching keywords' do
+      let(:str) { 'baked sprouts' }
+
+      it 'should include matching recipes' do
+        is_expected.to include recipe_1, recipe_2
+      end
+    end
+  end
 end
